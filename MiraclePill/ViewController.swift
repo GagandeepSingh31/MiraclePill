f56ec696758a9b75388fa90be26a88da3dda068d
Gagandeep Singh <gagandeepsingh@Gagandeeps-MacBook-Air.local>
2017-03-16 15:57:04 +0530

    we have main the UI and also add a state picker

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Rohini", "Rani Bagh", "Rajouri Garden", "Pritam Pura","Hari Nagar","Paschim Vihar"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.delegate = self
+        statePicker.dataSource = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +30,30 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        
+        statePicker.isHidden = false
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+    
 }
 
